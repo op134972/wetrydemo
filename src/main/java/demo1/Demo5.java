f64976b115de840055dcd64fed16e91d18849ae3
tangwenchuan <tangwenchuan@kanzhun.com>
2017-11-21 13:26:09 +0800

    提交修改

--- src/main/java/demo1/Demo5.java
+++ src/main/java/demo1/Demo5.java
@@ -1,15 +1,28 @@
 package demo1;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Demo5 {
+    public static final Pattern PhoneNoPatten = Pattern.compile("^1[0-9]{10}$");
 
+    public static boolean isPhoneNo(String phone){
+        boolean result = false;
+        if (StringUtils.isNotEmpty(phone)) {
+            result = PhoneNoPatten.matcher(phone).matches();
+        }
+        return result;
+    }
     static final List<Integer> list = new ArrayList<>();
 
     public static void main(String[] args) {
-        for (int i = 0; i < 10000; i++) {
-            list.add(i);
-        }
+//        for (int i = 0; i < 10000; i++) {
+//            list.add(i);
+//        }
+
+        System.out.println(isPhoneNo("12478558554"));
     }
 }
--- src/main/java/javaapi/SimpleDateFormatTest.java
+++ src/main/java/javaapi/SimpleDateFormatTest.java
@@ -7,7 +7,7 @@ public class SimpleDateFormatTest {
     private static final ThreadLocal<SimpleDateFormat> df = new ThreadLocal<SimpleDateFormat>(){
         @Override
         protected SimpleDateFormat initialValue() {
-            return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            return new SimpleDateFormat("yyyy-MM-McpController HH:mm:ss");
         }
     };
 
