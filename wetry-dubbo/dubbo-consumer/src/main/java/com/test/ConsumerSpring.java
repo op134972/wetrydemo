51fb0173e411a39e5d68ad44a07faf7e5c244939
tangwenchuan <tangwenchuan@kanzhun.com>
2018-03-30 22:01:17 +0800

    dubbo

--- wetry-dubbo/dubbo-consumer/src/main/java/com/test/ConsumerSpring.java
+++ wetry-dubbo/dubbo-consumer/src/main/java/com/test/ConsumerSpring.java
@@ -1,7 +1,17 @@
 package com.test;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Created by wch on 18-3-30.
  */
 public class ConsumerSpring {
+
+    public static void main(String[] args) throws InterruptedException {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring/spring-dubbo-consumer.xml");
+
+        System.out.println("consumer begin...");
+        Speaker speaker = (Speaker) context.getBean("speaker", Speaker.class);
+        System.out.println(speaker.speak("hello"));
+    }
 }
--- wetry-dubbo/dubbo-provider/src/main/java/com/test/ProviderSpring.java
+++ wetry-dubbo/dubbo-provider/src/main/java/com/test/ProviderSpring.java
@@ -1,11 +1,16 @@
 package com.test;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Created by wch on 18-3-30.
  */
 public class ProviderSpring {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring/spring-dubbo-provider.xml");
 
+        System.out.println("begin...");
+        Thread.currentThread().join();
     }
 }
