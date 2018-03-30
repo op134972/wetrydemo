60a7f7dcbba2567885fae1db931b46146ab1e141
tangwenchuan <tangwenchuan@kanzhun.com>
2018-03-30 22:23:51 +0800

    dubbo

--- wetry-dubbo/dubbo-provider/src/main/java/com/test/SpeakerImpl.java
+++ wetry-dubbo/dubbo-provider/src/main/java/com/test/SpeakerImpl.java
@@ -1,8 +1,11 @@
 package com.test;
 
+import org.springframework.stereotype.Service;
+
 /**
  * Created by wch on 18-3-30.
  */
+@Service
 public class SpeakerImpl implements Speaker {
     @Override
     public String speak(String words) {
