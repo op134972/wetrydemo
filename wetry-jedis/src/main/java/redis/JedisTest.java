a8666291f41af1706a587dd3b5d8e47b6f5d4c66
tangwenchuan <tangwenchuan@kanzhun.com>
2018-04-13 22:07:50 +0800

    dubbo提交

---
+++ wetry-jedis/src/main/java/redis/JedisTest.java
@@ -0,0 +1,22 @@
+package redis;
+
+import redis.clients.jedis.Jedis;
+
+import java.util.Set;
+
+/**
+ * Created by wch on 18-4-11.
+ */
+public class JedisTest {
+    public static void main(String[] args) {
+        Jedis jedis = new Jedis("127.0.0.1",6379);
+
+        System.out.println(jedis);
+
+        Set<String> keys = jedis.keys("*");
+
+        jedis.set("one","1");
+
+        System.out.println(jedis.get("one"));
+    }
+}
